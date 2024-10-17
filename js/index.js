17d06004bc9847b059887c99d93107da16be9fd4
Juno <jvmeifert@gmail.com>
2024-10-17 10:14:03 -0400

    Use jquery, add expandable about me section

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function toggle_nav_expand() {
-    const nav = document.querySelector("nav");
-    if(nav.classList.contains("expanded")) {
-        nav.classList.remove("expanded");
-    } else {
-        nav.classList.add("expanded");
-    }
-}
